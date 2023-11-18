3aa128d07bf5d5b27daac7a741a238e6ce71faa4
Saleh Ahmed <saleh@alliancebd.net>
2023-11-18 16:16:19 +0600

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { FaInstagram, FaBehanceSquare, FaGithub, FaHome, FaUser, FaBookOpen, FaPencilRuler } from "react-icons/fa";
-import { motion } from "framer-motion"
+import { FaInstagram, FaBehanceSquare, FaGithub, FaHome, FaUser, FaBookOpen, FaPencilRuler, FaEnvelope } from "react-icons/fa";
 import bgVideo from '../assets/background.mp4'
 
 export default function home() {
@@ -8,7 +7,7 @@ export default function home() {
     <div className='master-container h-screen w-screen flex justify-center items-center p-4 bg-black/75'>
         <video src={bgVideo} className='absolute object-cover h-screen w-screen -z-10 top-0 left-0 overflow-x-hidden' autoPlay loop muted></video>
         <div className="home h-full w-full rounded-xl flex shadow-xl text-white">
-            <div className="info-container w-3/12 min-w-fit border border-neutral-500 rounded-2xl p-16 flex flex-col justify-between">
+            <div className="info-container w-3/12 min-w-fit border border-neutral-500 rounded-2xl p-16 hidden lg:flex flex-col justify-between">
                 <div className="intro-text">
                     <h3 className='text-neutral-400 font-medium'>Designer & Developer</h3>
                     <h1 className='font-light text-4xl'>Saleh Ahmed</h1>
@@ -28,21 +27,22 @@ export default function home() {
                     <button className='bg-accent border border-neutral-500 rounded-full w-full h-12 text-black font-bold hover:bg-black hover:text-white'>Hire Me</button>
                 </div>
             </div>
-            <div className="content-container bg-neutral-5000 w-9/12 px-32 py-16 overflow-y-scroll">
+            <div className="content-container w-full lg:w-9/12 px-2 lg:px-32 py-16 overflow-y-scroll">
                 <div className="introduction mb-32">
                     <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaHome />Introduction</h3>
-                    <h1 className='mb-10 text-6xl font-medium'>Hi. This is <span className='text-accent'>Saleh</span>, <br/>Designer and Developer.</h1>
+                    <h1 className='mb-10 text-4xl md:text-6xl font-medium'>Hi. This is <span className='text-accent'>Saleh</span>, <br/>Designer and Developer.</h1>
                     <p className='text-neutral-400'>I design minimal design elements, and I love what I do.<br/> It's simple like that!</p>
                 </div>
                 <div className="about mb-32">
                     <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaUser />About</h3>
-                    <h2 className='mb-10 text-4xl'>Every great design begins with<br/> an even <span className='text-accent'>better story</span></h2>
-                    <p className='text-neutral-400 w-3/4 mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt eligendi vel, quibusdam sit quo culpa voluptates, quisquam at optio libero ducimus distinctio voluptas veritatis modi non nesciunt commodi molestiae! Possimus?</p>
+                    <h2 className='mb-10 text-2xl md:text-4xl'>Every great design begins with<br/> an even <span className='text-accent'>better story</span></h2>
+                    <p className='text-neutral-400 w-full md:w-3/4 mb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt eligendi vel, quibusdam sit quo culpa voluptates, quisquam at optio libero ducimus distinctio voluptas veritatis modi non nesciunt commodi molestiae! Possimus?</p>
                     <p className='mb-5 border-b w-fit'>What I Do</p>
-                    <ul className='flex gap-8'>
+                    <ul className='flex gap-8 flex-wrap'>
                         <li className=''>Web Design</li>
-                        <li>Frontend Web Development</li>
+                        <li>Web Development</li>
                         <li>UI/UX</li>
+                        <li>Framer Animations</li>
                     </ul>
                 </div>
                 <div className="experience mb-32">
@@ -60,16 +60,16 @@ export default function home() {
                     <h2 className='mb-10 text-4xl'><span className='text-accent'>Featured</span> Projects</h2>
                     <div className="projects-container flex flex-col gap-8">
                         <div className="project-1">
-                            <div className="display-image h-64 w-1/2 min-w-fit rounded-xl bg-black mb-3 cursor-pointer flex justify-start items-end gap-3 p-5">
+                            <div className="display-image h-64 w-full  lg:w-1/2 rounded-xl bg-black mb-3 cursor-pointer flex flex-col md:flex-row justify-start items-end flex-wrap gap-3 px-3">
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>Tensorflow.js</p>
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>YOLOV4</p>
-                                <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>Android Studio</p>
+                                <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium whitespace-nowrap'>Android Studio</p>
                             </div>
                             <h3>Project Drishti</h3>
                             <p className='text-neutral-400'>App for the visually impaired</p>
                         </div>
                         <div className="project-2">
-                            <div className="display-image h-64 w-1/2 min-w-fit rounded-xl bg-black mb-3 cursor-pointer flex justify-start items-end gap-3 p-5">
+                            <div className="display-image h-64 w-full min-w-fit lg:w-1/2 rounded-xl bg-black mb-3 cursor-pointer flex flex-col md:flex-row  justify-start items-end gap-3 p-5">
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>Figma</p>
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>MERN</p>
                             </div>
@@ -77,7 +77,7 @@ export default function home() {
                             <p className='text-neutral-400'>Team managemnt software</p>
                         </div>
                         <div className="project-3">
-                            <div className="display-image h-64 w-1/2 min-w-fit rounded-xl bg-black mb-3 cursor-pointer flex justify-start items-end gap-3 p-5">
+                            <div className="display-image h-64 w-full min-w-fit lg:w-1/2 rounded-xl bg-black mb-3 cursor-pointer flex flex-col md:flex-row  justify-start items-end gap-3 p-5">
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>Figma</p>
                                 <p className='border h-fit w-fit px-3 py-1 rounded-full bg-white text-black font-medium'>React</p>
                             </div>
@@ -89,8 +89,8 @@ export default function home() {
                 <div className="stacks mb-32">
                     <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaPencilRuler />My Skills</h3>
                     <h2 className='mb-10 text-4xl'>Technology <span className='text-accent'>Stacks</span></h2>
-                    <div className="tech-stacks flex justify-start items-start flex-wrap gap-5">
-                        <div className="stack-1 rounded h-20 w-48 min-w-fit bg-white" id='Figma'></div>
+                    <div className="tech-stacks flex justify-start items-start flex-wrap gap-5 w-full md:w-3/4">
+                        <div className="stack-1 rounded h-20 w-48 bg-white" id='Figma'></div>
                         <div className="stack-1 rounded h-20 w-48 bg-white" id='React'></div>
                         <div className="stack-1 rounded h-20 w-48 bg-white" id='Node'></div>
                         <div className="stack-1 rounded h-20 w-48 bg-white" id='MongoDB'></div>
@@ -98,9 +98,13 @@ export default function home() {
                         <div className="stack-1 rounded h-20 w-48 bg-white" id=''></div>
                     </div>
                 </div>
-                <div className="contact mb-32"></div>
+                <div className="contact mb-16">
                     <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaPencilRuler />Contact</h3>
                     <h2 className='mb-10 text-4xl'>Let's have a cup of <span className='text-accent'>coffee</span></h2>
+                    <p className='w-full md:w-3/4'>Dont hesitate to get in touch. Lets talk about all about your bright ideas, we can bring to life. Together. </p>
+                    <p className='w-full md:w-3/4 mb-12'>You can reach out to me at:</p>
+                    <a className='flex justify-center items-center gap-3 w-fit border rounded-full p-5 hover:bg-green-500 hover:text-black hover:border-green-500 duration-300' href="mailto:ahmedsalehdhk@gmail.com"><FaEnvelope />ahmedsalehdhk@gmail.com</a>
+                </div>
             </div>
         </div>
     </div>
