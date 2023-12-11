360ce5b536eaf9757cd5141a1a6b8a159b585d9e
Saleh Ahmed <saleh@alliancebd.net>
2023-12-11 17:02:05 +0600

    Near Completion

---
+++ client/src/animations/animation.js
@@ -0,0 +1,15 @@
+
+export const variants = {
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeInOut',
+        duration: 0.5
+      }
+    },
+    hide: {
+      y: 50,
+      opacity: 0
+    }
+  };
