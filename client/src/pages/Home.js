50bb64ab5e4994c91164ea7d51571b4f7dfc175f
Saleh Ahmed <saleh@alliancebd.net>
2023-12-12 13:16:16 +0600

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { easeInOut, motion } from "framer-motion"
-import { FaInstagram, FaBehanceSquare, FaGithub, FaHome, FaUser, FaBookOpen, FaPencilRuler, FaPaintBrush, FaEnvelope } from "react-icons/fa";
-import bgVideo from '../assets/background.mp4'
+import { FaInstagram, FaBehanceSquare, FaGithub, FaEnvelope } from "react-icons/fa";
+import bgVideo from '../assets/background.mp4';
+import block from '../assets/block.png'
 import { variants } from '../animations/animation';
 
 export default function Home() {
@@ -41,34 +42,41 @@ export default function Home() {
         <div className="home h-full w-full rounded-xl flex shadow-xl text-white">
             {/* INFO */}
             <motion.div className="info-container w-3/12 min-w-fit h-full min-h-fit border border-neutral-500 rounded-2xl p-16 hidden lg:flex flex-col justify-between gap-5 overflow-y-hidden" initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{ease: easeInOut, duration: 0.5}} >
-                <div className="intro-text">
+                
+                <div className="intro-text mb-5">
                     <h3 className='text-neutral-400 font-medium'>Designer & Engineer</h3>
                     <h1 className='font-light text-4xl'>Saleh Ahmed</h1>
                 </div>
-                <div className="image-container bg-neutral-900 rounded-full h-52 w-52 min-h-[250px] min-w-[250px] self-center flex justify-center items-center bg-cover bg-center"></div>
-                <div className="contact-text mb-20 text-center">
-                    <h2 className='font-medium text-[1.2rem]'>CSE, NSU</h2>
-                    <h2 className='font-medium text-[1.2rem] mb-5'>ahmedsalehdhk@gmail.com</h2>
-                    <h3 className='text-neutral-400 font-medium'>Dhaka, Bangladesh</h3>
+                <div className='profile flex flex-col'>
+                    <div className="image-container bg-neutral-900 rounded-full h-32 w-32 min-h-[200px] min-w-[200px] self-center flex justify-center items-center bg-cover bg-center mb-5"></div>
+                    <div className="contact-text mb-20 text-center">
+                        <h2 className='font-medium text-[1.2rem]'>CSE, NSU</h2>
+                        <h2 className='font-medium text-[1.2rem] mb-5'>ahmedsalehdhk@gmail.com</h2>
+                        <h3 className='text-neutral-400 font-medium'>Dhaka, Bangladesh</h3>
+                    </div>
                 </div>
-                <div className="social-links flex justify-center gap-5">
-                    <a href='https://github.com/ahmedsalehdhk' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaGithub size={25} /></a>
-                    <a href='https://www.instagram.com/ahmedsalehdhk2023' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaInstagram size={25} /></a>
-                    <a href='https://www.behance.net/ahmedsalehdhk' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaBehanceSquare size={25} /></a>
-                </div>
-                <div className="button-container">
-                    <motion.button onClick={executeScroll} whileTap={{scale: 0.9}} className='bg-accent border border-neutral-500 rounded-full w-full h-12 text-black font-bold '>Hire Me</motion.button>
+                <div className='socials'>
+                    <div className="social-links flex justify-center gap-5 mb-5">
+                        <a href='https://github.com/ahmedsalehdhk' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaGithub size={25} /></a>
+                        <a href='https://www.instagram.com/ahmedsalehdhk2023' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaInstagram size={25} /></a>
+                        <a href='https://www.behance.net/ahmedsalehdhk' className='border border-neutral-400 rounded-full h-16 w-16 hover:bg-black flex justify-center items-center'><FaBehanceSquare size={25} /></a>
+                    </div>
+                    <div className="button-container">
+                        <motion.button onClick={executeScroll} whileTap={{scale: 0.9}} className='bg-accent border border-neutral-500 rounded-full w-full h-12 text-black font-bold '>Hire Me</motion.button>
+                    </div>
                 </div>
             </motion.div>
             {/* CONTENT */}
             <div className="content-container w-full lg:w-9/12 px-2 lg:px-32 py-16 overflow-y-scroll">
                 <motion.div className="introduction mb-32" initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{ease: easeInOut, duration: 0.8, delay: 1}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaHome />Introduction</h3>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>Introduction</h3>
                     <h1 className='mb-10 text-4xl md:text-6xl font-medium'>Hi. This is <span className='text-accent'>Saleh</span>,<br/><motion.div className='capitalize whitespace-nowrap text-2xl sm:text-4xl md:text-5xl lg:text-6xl' key={index} variants={variants} initial="hide" animate={'show'}>{ChangingTitles()}</motion.div></h1>
                     <p className='text-neutral-400'>I design and build minimal, beautiful software.<br/>It's simple and I love it!</p>
                 </motion.div>
                 <motion.div className="about mb-32" initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{ease: easeInOut, duration: 0.8, delay: 1.2}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaUser />About</h3>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>About</h3>
                     <h2 className='mb-10 text-2xl md:text-4xl'>Every great design begins with<br/> an even <span className='text-accent'>better story</span></h2>
                     <p className='text-neutral-400 w-full md:w-3/4 mb-5 md:text-justify'>My creative journey echoes the wonder of discovering storytelling through my sketchbooks as a child. Today, each project is a canvas to recreate that magic, infusing every pixel with purpose. Join me in this exciting landscape, where code and design elements converge, contributing to stories waiting to unfold.</p>
                     <p className='mb-5 border-b w-fit'>What I Do</p>
@@ -81,8 +89,8 @@ export default function Home() {
                     </ul>
                 </motion.div>
                 <motion.div className="experience mb-32" initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ease: easeInOut, duration: 0.8}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaBookOpen />Background</h3>
-                    <h2 className='mb-10 text-4xl'>Education and <span className='text-accent'>Experience</span></h2>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>Background</h3>                    <h2 className='mb-10 text-4xl'>Education and <span className='text-accent'>Experience</span></h2>
                     <ul className='flex flex-col gap-8'>
                         <li>Executive - Alliance Properties Ltd.<br/><span className='text-accent'>2023 - Present</span></li>
                         <li>Bachelors Degree on Computer Science & Technology<br/><span>North South University</span><br/><span className='text-neutral-400'>2018 - 2023</span></li>
@@ -91,8 +99,8 @@ export default function Home() {
                     </ul>
                 </motion.div>
                 <motion.div className="projects mb-32" initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ease: easeInOut, duration: 0.8}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaPencilRuler />Portfolio</h3>
-                    <h2 className='mb-10 text-4xl'><span className='text-accent'>Featured</span> Projects</h2>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>Portfolio</h3>                    <h2 className='mb-10 text-4xl'><span className='text-accent'>Featured</span> Projects</h2>
                     <div className="projects-container flex flex-col gap-8">
                         <motion.div className="project-1" initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ease: easeInOut, duration: 0.8}} viewport={{ once: true }}>
                             <div className='h-64 w-full lg:w-3/4 rounded-xl overflow-hidden mb-3 relative'>
@@ -131,25 +139,25 @@ export default function Home() {
                     </div>
                 </motion.div>
                 <motion.div className="stacks mb-32"  initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ease: easeInOut, duration: 0.8}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaPaintBrush />My Skills</h3>
-                    <h2 className='mb-10 text-4xl'>Technology <span className='text-accent'>Stacks</span></h2>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>My Skills</h3>                    <h2 className='mb-10 text-4xl'>Technology <span className='text-accent'>Stacks</span></h2>
                     <motion.div ref={carousel} className='outer-carousel cursor-grab outline-none w-full md:w-3/4 overflow-hidden' whileTap={{ cursor: "grabbing" }}>
                         <motion.div className="tech-stacks inner-carousel inline-flex gap-3" drag="x" dragConstraints={carousel}>
                             <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='figma'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='illustrator'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='mongoDB'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='express'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='react'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='nodeJS'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='reactnative'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='tailwindcss'></div>
-                            <div className="stack-1 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='framer'></div>
+                            <div className="stack-2 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='illustrator'></div>
+                            <div className="stack-3 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='mongoDB'></div>
+                            <div className="stack-4 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='express'></div>
+                            <div className="stack-5 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='react'></div>
+                            <div className="stack-6 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='nodeJS'></div>
+                            <div className="stack-7 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='reactnative'></div>
+                            <div className="stack-8 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='tailwindcss'></div>
+                            <div className="stack-9 rounded h-20 min-h-[100px] w-48 min-w-[180px] bg-white" id='framer'></div>
                         </motion.div>
                     </motion.div>
                 </motion.div>
                 <motion.div className="contact mb-16" id='contact' ref={contactRef}  initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}}  transition={{ease: easeInOut, duration: 0.8}} viewport={{ once: true }}>
-                    <h3 className='mb-16 border w-fit px-5 py-1 rounded-full uppercase flex justify-center items-center gap-2'><FaEnvelope />Contact</h3>
-                    <h2 className='mb-10 text-4xl'>Get In <span className='text-accent'>Touch</span></h2>
+                    <img src={block} alt="" className='h-4 mb-2' />
+                    <h3 className='mb-16 w-fit uppercase whitespace-nowrap'>Contact</h3>                    <h2 className='mb-10 text-4xl'>Get In <span className='text-accent'>Touch</span></h2>
                     <p className='w-full md:w-3/4 inline-block'>Say Hello. Lets talk over coffee, perhaps?</p>
                     <p className='w-full md:w-3/4 mb-12'>You can reach out to me at:</p>
                     <motion.a whileHover={{scale: 1.1}} whileTap={{scale: 0.9}} className='flex justify-center items-center gap-2 md:gap-3 w-fit border rounded-full px-3 md:px-5 p-5 hover:bg-green-500 hover:text-black hover:border-green-500 duration-300' href="mailto:ahmedsalehdhk@gmail.com"><FaEnvelope />ahmedsalehdhk@gmail.com</motion.a>
